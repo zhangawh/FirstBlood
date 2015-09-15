7abec09834f6f1ccfdf6203b77249b67f51d6225
ABAO <zwh.abao@foxmail.com>
2015-09-15 09:23:51 +0800

    Write a test file

---
+++ winePredict.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 14 22:18:51 2015
+
+@author: zhangwh
+"""
+
+import pandas as pd
+df = pd.read_csv('winequality-red.csv', sep = ';')
+print df.describe()
